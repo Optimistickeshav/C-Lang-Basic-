b606a8fb3577b97a77793094c3a7a1ca91da7ffe
Keshav Verma <54373529+Optimistickeshav@users.noreply.github.com>
2021-07-18 18:33:42 +0530

    Add files via upload

--- 19_sorting_array.c
+++ 19_sorting_array.c
@@ -7,18 +7,41 @@ int main(){
 
 int i, temp, swap;
 int n = 10;
-int goals(n);
+int goals[10];
 
 for(i=0; i<n; i++){
 goals[i] = (rand()%25)+1;
 }
 
- 
+printf("List\n");
+
 for(i=0; i<n; i++){
-goals[i] = (rand()%25)+1;
+printf("%d \n", goals[i]);
 }
 
+while(1){
+	swap = 0;
+	for(i=0; i<n-1; i++){
+		if(goals[i]>goals[i+1]){
+		int temp = goals[i];
+		goals[i] = goals[i+1];
+		goals[i+1] = temp;
+		swap = 1;
+	
+								}
 
+						}
+	if(swap==0){
+	break;
+				}
+
+		}
+
+printf("\nSorted List\n");
+for(i=0; i<n; i++){
+printf("%d \n", goals[i]);
+
+}
 
 return 0;
 }
